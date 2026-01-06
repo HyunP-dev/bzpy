345548973ad9ac5a0594f05aa166a04905819ff3
Hyun Park <hyunp.dev@gmail.com>
2026-01-06 15:03:13 +0900

    initial upload

---
+++ src/bzpy/__init__.py
@@ -0,0 +1,6 @@
+"""Bandizip wrapper for python"""
+
+from .bz import ArchiveFile, EntryInfo
+
+
+__all__ = ["ArchiveFile", "EntryInfo"]
---
+++ src/bzpy/bz.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+import subprocess
+import re
+
+
+class ArchiveFile:
+    def __init__(self, path: str):
+        self.path = path
+
+    def infolist(self) -> list[EntryInfo]:
+        """Return a list of class EntryInfo instances for files in the archive."""
+        p = subprocess.Popen(["bz", "l", self.path],
+                             stdout=subprocess.PIPE, text=True)
+        stdout, stderr = p.communicate()
+        infolist = []
+        for line in stdout.splitlines()[6:-2]:
+            date_, time_, attr, file_size, compress_size, filename = re.split(
+                " +", line, maxsplit=5)
+            date_time = date_ + " " + time_
+            file_size = int(file_size)
+            compress_size = int(compress_size)
+            entry_info = EntryInfo(date_time, attr,
+                                   file_size, compress_size, filename)
+            infolist.append(entry_info)
+        return infolist
+
+    def namelist(self) -> list[str]:
+        """Return a list of file names in the archive."""
+        p = subprocess.Popen(["bz", "l", "-list:s", self.path],
+                             stdout=subprocess.PIPE, text=True)
+        stdout, stderr = p.communicate()
+        return stdout.splitlines()
+
+
+class EntryInfo:
+    def __init__(self, date_time: str, attr: str,
+                 file_size: int, compress_size: int, filename: str):
+        self.date_time = date_time
+        self.attr = attr
+        self.file_size = file_size
+        self.compress_size = compress_size
+        self.filename = filename
+
+    def __repr__(self):
+        infos = []
+        for name in ["filename", "file_size", "compress_size"]:
+            infos.append(name + "=" + repr(getattr(self, name)))
+        return f"<EntryInfo {' '.join(infos)}>"
